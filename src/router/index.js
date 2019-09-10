90bfc1483f092facce020b9c8261bf988a1d7c5e
wangliheng <springioc@yeah.net>
2019-09-10 18:19:29 +0800

    add login

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/login',
+      name: 'login',
+      meta: {
+        title: 'Login - 登录',
+        hideInMenu: true
+      },
+      component: () => import('@/views/login/login.vue')
+    },
   ]
 })
---
+++ src/views/login/login.vue
@@ -0,0 +1,42 @@
+<style lang="less">
+  @import './login.less';
+</style>
+
+<template>
+  <div class="login">
+    <div class="login-con">
+      <Card icon="log-in" title="欢迎登录" :bordered="false">
+        <div class="form-con">
+          <login-form @on-success-valid="handleSubmit"></login-form>
+          <p class="login-tip">输入任意用户名和密码即可</p>
+        </div>
+      </Card>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/login-form/login-form'
+
+export default {
+  components: {
+    LoginForm
+  },
+  methods: {
+    handleSubmit ({ userName, password }) {
+      this.handleLogin({ userName, password }).then(res => {
+        this.getUserInfo().then(res => {
+          this.$router.push({
+              // todo 跳转到首页
+            //name: this.$config.homeName
+          })
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
